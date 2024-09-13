d07f95b6ef3209f0a3b95a7c88f4fe21cb0c7c11
veerasaranen <veera.saranen@aalto.fi>
2024-09-13 14:47:00 +0300

    exercices 2.16.-2.17.

--- part2/dataForCountries/src/App.jsx
+++ part2/dataForCountries/src/App.jsx
@@ -1,19 +1,34 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+import Notification from './components/Notification'
 import noteService from './services/notes'
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
+    </div>
+  )
+}
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
       .getAll()
       .then(initialNotes => {
         setNotes(initialNotes)
-      }) 
+      })
   }, [])
 
   const addNote = (event) => {
@@ -24,11 +39,30 @@ const App = () => {
     }
   
     noteService
-    .create(noteObject)
-    .then(returnedNote => {
-      setNotes(notes.concat(returnedNote))
-      setNewNote('')
-    })
+      .create(noteObject)
+        .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService
+      .update(id, changedNote)
+        .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      })
   }
 
   const handleNoteChange = (event) => {
@@ -39,25 +73,10 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
-  const toggleImportanceOf = id => {
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
-    
-    noteService
-      .update(id, changedNote).then(returnedNote => {
-        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-      })
-      .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
-        )
-        setNotes(notes.filter(n => n.id !== id))
-      })
-  }
-
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
@@ -65,9 +84,9 @@ const App = () => {
       </div>      
       <ul>
         {notesToShow.map(note => 
-          <Note 
-            key={note.id} 
-            note={note} 
+          <Note
+            key={note.id}
+            note={note}
             toggleImportance={() => toggleImportanceOf(note.id)}
           />
         )}
@@ -79,6 +98,7 @@ const App = () => {
         />
         <button type="submit">save</button>
       </form> 
+      <Footer />
     </div>
   )
 }
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,6 +3,8 @@ import SearchField from './components/search.jsx'
 import AddPeople from './components/addPeople.jsx'
 import People from './components/people.jsx'
 import peopleService from './services/people.js'
+import Notification from './components/notification.jsx'
+import Error from './components/error.jsx'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -10,6 +12,8 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
   const [filtered, setNewFiltered] = useState(persons)
+  const [notification, setNewNotification] = useState(null)
+  const [error, setNewError] = useState(null)
 
   const hook = () => {
     peopleService
@@ -39,7 +43,10 @@ const App = () => {
           setNewNumber("")
           setNewName("")
         })
-
+      setNewNotification(`${newName} was successfully added to contacts`)
+      setTimeout(() => {
+        setNewNotification(null)
+      }, 5000)
     } else {
       //alert(`${newName} is already added to phonebook`), i commented the old exercice
       if (window.confirm(`${newName} is already added to phonebook. Do you want to replace the old contact?`)) {
@@ -52,8 +59,22 @@ const App = () => {
             setNewFiltered(filtered.map(person => person.id !== changing.id ? person : returnedPerson))
             setNewNumber("")
             setNewName("")
+            setNewNotification(`${newName} was changed`)
+            setTimeout(() => {
+              setNewNotification(null)
+            }, 5000)
+          })
+          .catch(error => {
+            setNewError(`Oops, ${newName} was already deleted from server`)
+            setTimeout(() => {
+              setNewNotification(null)
+            }, 5000)
           })
       } else {
+        setNewNotification(`changes to ${newName} were canceled`)
+        setTimeout(() => {
+          setNewNotification(null)
+        }, 5000)
         setNewNumber("")
         setNewName("")
       }
@@ -83,12 +104,23 @@ const App = () => {
         .deleting(person.id)
       setPersons(persons.filter(dude => dude.id !== person.id))
       setNewFiltered(filtered.filter(dude => dude.id !== person.id))
+      setNewNotification(`${person.name} was deleted from contacts`)
+      setTimeout(() => {
+        setNewNotification(null)
+      }, 5000)
+    } else {
+      setNewNotification(`deletion of ${person.name} was canceled`)
+      setTimeout(() => {
+        setNewNotification(null)
+      }, 5000)
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Error message={error} />
+      <Notification message={notification} />
       <SearchField input={newSearch} filter={filterSearch}/>
       <h3>Add a contact</h3>
       <AddPeople person={addPerson} name={newName} addName={handleNames} number={newNumber} addNumber={handleNumbers}/>
