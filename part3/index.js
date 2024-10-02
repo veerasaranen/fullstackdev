950e1548a51c552ece663eeee18b6f83aec1e7ed
veerasaranen <veera.saranen@aalto.fi>
2024-10-02 15:28:59 +0300

    exercices 3.9-3.11

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', (request, response) => {
   if (request.method === 'POST') {
@@ -107,7 +111,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
